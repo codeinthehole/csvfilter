73d4db739450accaad01ffaef1ae0b1a4de0d4e9
David Winterbottom <david.winterbottom@gmail.com>
2012-04-01 21:31:56 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@
 from setuptools import setup, find_packages
 from csvfilter import VERSION
 
+
 setup(name='csvfilter',
       version=VERSION,
       url='https://github.com/codeinthehole/csvfilter',
       author="David Winterbottom",
       author_email="david.winterbottom@tangentlabs.co.uk",
-      description="A command-line tool and Python API for manipulating CSV data, eg plucking columns and reordering them",
+      description="A command-line utility and Python API for manipulating CSV data, eg plucking columns and reordering them.  It's a bit like the unix utility 'cut'",
       long_description=open('README.rst').read(),
-      keywords="CSV",
-      platforms=['linux'],
       packages=find_packages(),
-      scripts = ['bin/csvfilter'])
+      scripts=['bin/csvfilter'])
